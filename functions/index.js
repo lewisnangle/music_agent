e22034af0a740bf4c29b4ad86ec357821a589d1b
lewisnangle <2144120n@student.gla.ac.uk>
2017-12-01 14:11:22 +0000

    Implemented the basic Skiddle artist search

--- functions/index.js
+++ functions/index.js
@@ -134,42 +134,40 @@ function findSkiddleArtist(artist){
 
   function findBasicEvent (app) {
 
-    let city = app.getArgument(CITY_ARGUMENT);
+    let city = app.getArgument(CITY_ARGUMENT);          //get specified city argument from dialogFlow
 
-    getGeoData(city).then(function(r){
+    getGeoData(city).then(function(r){                  //get the geodata from the specified city using Google API
         var data = r;
 
         JSON.stringify(data);
 
-        console.log("geo : " + data);
-
+        //get latitude and longitude from geodata
         var latitude = data[0]['latitude'];
         var longitude = data[0]['longitude'];
 
-   //     app.tell('Latitude & Longitude : ' + latitude + " , " + longitude);
 
-        getBasicSkiddleEvents(latitude,longitude).then(function (res) {
+        getBasicSkiddleEvents(latitude,longitude).then(function (res) {     //use latitude and longitude with skiddle API to return an events object
             var data = res;
 
-            var results = JSON.parse(data).results;
-            var pageCount = JSON.parse(data).pagecount;
+            var results = JSON.parse(data).results;                    //get results of events object
+            var pageCount = JSON.parse(data).pagecount;                //get page count
 
-            var eventList = [];
+            var eventList = [];                                         //list to hold event names
 
             for(let i = 0; i<pageCount;i++){
-                eventList.push(results[i]['eventname']);
+                eventList.push(results[i]['eventname']);                //put events in list
             }
 
-            var eventString = eventList.join(',  ');
+            var outputString = eventList.join(',  ');                    //turn events into string to be outputted through Dialogflow
 
-            app.tell("Here is a list of events happening in " + city + ": " + eventString);
+            app.tell("Here is a list of events happening in " + city + ": " + outputString); //provide response to user
 
         }).catch(function (err) {
-            console.log("Error Occurred :" + err);
+            console.log("Error Occurred :" + err);          //catch errors from SkiddleEvents function
         });
 
-    }).catch(function(e){
-        console.log('error occurred: ' + e)
+    }).catch(function(err){
+        console.log('error occurred: ' + err)               //catch errors from geodata function
         }
     )
   }
@@ -186,14 +184,32 @@ function findSkiddleArtist(artist){
         var pageCount = JSON.parse(data).pagecount;
 
 
-        var eventDict = {};
+        var eventDict = {};             //dictionary of events, where key is the venue name and value is the act/name of the event
 
-        for(let i = 0; i<pageCount;i++){
-            eventDict[results[i]['venue']['name']] = results[i]['eventname'];
+        for(let i = 0; i<pageCount;i++) {
+            eventDict[results[i]['venue']['name']] = results[i]['eventname'];      //fill dictionary with venue and event names
         }
 
 
-        app.tell(eventDict);
+        var eventList = []                  //list to hold venues and events names as list
+
+
+        for (var key in eventDict) {
+            if (eventDict.hasOwnProperty(key)) {
+                eventList.push( [key, eventDict[key]]);         //put each list of venue and event name in to list
+            }
+        }
+
+        var eventFormattedList = [];            //list to hold formatting
+
+
+        for (var i = 0; i <eventList.length; i++){
+            eventFormattedList.push(eventList[i].join(', is Hosting: '));      //format each list to say "venue, is Hosting: event"
+        }
+
+        var outputString = eventFormattedList.join(", \xa0");               //format string to be outputted
+
+        app.tell(outputString);                             //provide response to user
 
 
     }).catch(function(e){
