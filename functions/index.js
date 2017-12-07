78adc5de49aa200b1086a119308fdd3e9af7b04e
lewisnangle <2144120n@student.gla.ac.uk>
2017-12-07 18:39:20 +0000

    spotify auth not working

--- functions/index.js
+++ functions/index.js
@@ -46,8 +46,6 @@ var spotifyApp = admin.initializeApp({
 },"spotifyApp");
 
 
-
-
 // Spotify OAuth 2 setup
 // TODO: Configure the `spotify.client_id` and `spotify.client_secret` Google Cloud environment variables.
 const SpotifyWebApi = require('spotify-web-api-node');
@@ -106,6 +104,10 @@ exports.token = functions.https.onRequest((req, res) => {
                     // We have a Spotify access token and the user identity now.
                     const accessToken = data.body['access_token'];
                     const spotifyUserID = userResults.body['id'];
+
+                    var code = spotifyUserID + Math.floor(Math.random() * 100); //random code to give to user to get spotify access
+
+
                     var profilePic;
                     if ( (userResults.body['images'][0]) == undefined){         //if there is no profile picture defined, give user generic one.
                         profilePic = 'http://oi68.tinypic.com/255mgle.jpg';     //hosted default pic on tinypic.com
@@ -192,17 +194,10 @@ function writeUserData(name,musicGenres,artists) {
 
 
 
-function checkExists (user) {
-    var userRef = db.ref('users/');
-    userRef.orderByChild("username").equalTo(user).once("value",snapshot => {
-        const userData = snapshot.val();
-        if (userData){
-            console.log("EXISTS ALREADY.");
-        } else {
-            console.log("DOESNT EXIST GO ON");
-        }
-    });
-}
+
+
+
+
 
 
 
@@ -218,7 +213,8 @@ const App = require('actions-on-google').DialogflowApp;
 // a. the action name from the Dialogflow intent
 const FIND_BASIC_EVENTS_ACTION = 'find_basic_events';
 const FIND_ARTIST = 'find_artist';
-const WELCOME_FIRST_TIME_ACTION = 'Welcome.Welcome-no';
+const WELCOME_PICK_USERNAME = 'Welcome.Welcome-no';
+const SPOTIFY_LOGGED_IN = 'spotify_logged_in';
 
 // b. the parameters that are parsed from the make_name intent
 const ARTIST_ARGUMENT = 'music-artist';
@@ -229,56 +225,60 @@ const MUSIC_ARTISTS_ARGUMENT = 'music-artists';
 
 
 exports.MusicPlayer = functions.https.onRequest((request, response) => {
-const app = new App({request, response});
+    const app = new App({request, response});
 
 
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
 
-console.log('Request headers: ' + JSON.stringify(request.headers));
-console.log('Request body: ' + JSON.stringify(request.body));
+    var sessionID = request.body.sessionId;          //get the sessionId of the request.
+
+
+    console.log("Session ID in dialogflow... : " + sessionID);
 
 //------------------------------------------------------------Events and geocoding------------------------------
-var NodeGeocoder = require('node-geocoder');            //require node-geocoder for converting location names into coordinates
-var request = require('request');             //require the request module for calling the Skiddle API.
-var skiddleKey = '7b8ab215e66d06e23c5f8b6a691de015';
-var geoCodeKey = 'AIzaSyDHtzdPWL_4k-NFqrzV3LPA0DS1kxravII'
+    var NodeGeocoder = require('node-geocoder');            //require node-geocoder for converting location names into coordinates
+    var request = require('request');             //require the request module for calling the Skiddle API.
+    var skiddleKey = '7b8ab215e66d06e23c5f8b6a691de015';
+    var geoCodeKey = 'AIzaSyDHtzdPWL_4k-NFqrzV3LPA0DS1kxravII'
 
 
 
 
-var optionsGeo = {
-    provider: 'google',
-    httpAdapter: 'https', // Default
-    apiKey: geoCodeKey, // for Mapquest, OpenCage, Google Premier
-    formatter: null         // 'gpx', 'string', ...
-};
+    var optionsGeo = {
+        provider: 'google',
+        httpAdapter: 'https', // Default
+        apiKey: geoCodeKey, // for Mapquest, OpenCage, Google Premier
+        formatter: null         // 'gpx', 'string', ...
+    };
 
-var geocoder = NodeGeocoder(optionsGeo);
-var rp = require('request-promise');
+    var geocoder = NodeGeocoder(optionsGeo);
+    var rp = require('request-promise');
 
 
 
 //Get the geo data from the city name
-function getGeoData(city){
-    return new Promise(function(resolve,reject){
-        geocoder.geocode(city, function(err,res) {
-            if(res) //If no result, then error
-                resolve(res)
-            else
-                reject(err)
-        });
-    })
+    function getGeoData(city){
+        return new Promise(function(resolve,reject){
+            geocoder.geocode(city, function(err,res) {
+                if(res) //If no result, then error
+                    resolve(res)
+                else
+                    reject(err)
+            });
+        })
 
-};
+    };
 
 //get skiddle events from coordinates
-function getBasicSkiddleEvents(latitude,longitude){
+    function getBasicSkiddleEvents(latitude,longitude){
         return rp('https://www.skiddle.com/api/v1/events/search/?api_key=' + skiddleKey + '&latitude=' + latitude + '&longitude=' + longitude + '&radius=5&eventcode=LIVE&order=distance&description=1')
     }
 
 //get skiddle events from artist name
-function findSkiddleArtist(artist){
-    return rp('https://www.skiddle.com/api/v1/events/search/?api_key=' +  skiddleKey + '&keyword=' + artist)
-}
+    function findSkiddleArtist(artist){
+        return rp('https://www.skiddle.com/api/v1/events/search/?api_key=' +  skiddleKey + '&keyword=' + artist)
+    }
 
 
 //--------------------------------------------------------------------------------------------------------------------------
@@ -287,124 +287,134 @@ function findSkiddleArtist(artist){
 // c. The functions
 
 
-  function welcomeFirstTime (app) {
-      let name = app.getArgument(USERNAME_ARGUMENT);        //get name of user
-      let genres = app.getArgument(MUSIC_GENRES_ARGUMENT);           //get music genres user likes
-      let artists = app.getArgument(MUSIC_ARTISTS_ARGUMENT);
-
-      checkExists(name);
 
 
+    function spotifyLoggedIn (app) {
+        app.tell("You have logged into spotify");
+    }
 
-      writeUserData(name,genres,artists);
+    function welcomePickUsername (app) {
+        let nameUserWants = app.getArgument(USERNAME_ARGUMENT);        //get name of user
 
-      app.tell('You wont be forgotten ' + name + ". \xa0" + "The genres you like are : " + genres + " The artists you like are: " + artists );
-
-  }
+        let genres = 'rap';
+        let artists = 'acdc';
 
 
 
-  function findBasicEvent (app) {
-
-    let city = app.getArgument(CITY_ARGUMENT);          //get specified city argument from dialogFlow
-
-    getGeoData(city).then(function(r){                  //get the geodata from the specified city using Google API
-        var data = r;
-
-        JSON.stringify(data);
-
-        //get latitude and longitude from geodata
-        var latitude = data[0]['latitude'];
-        var longitude = data[0]['longitude'];
-
-
-        getBasicSkiddleEvents(latitude,longitude).then(function (res) {     //use latitude and longitude with skiddle API to return an events object
-            var data = res;
-
-            var results = JSON.parse(data).results;                    //get results of events object
-            var pageCount = JSON.parse(data).pagecount;                //get page count
-
-            var eventList = [];                                         //list to hold event names
-
-            for(let i = 0; i<pageCount;i++){
-                eventList.push(results[i]['eventname']);                //put events in list
+        var userRef = db.ref('users/');
+        userRef.orderByChild("username").equalTo(nameUserWants).once("value",snapshot => {     //check if username already exists
+            const userData = snapshot.val();
+            if (userData){
+                app.tell(nameUserWants + " already exists, can you pick another one.");
+            } else {
+                writeUserData(nameUserWants,genres,artists);
+                app.tell(nameUserWants + ", I have remembered your music taste.");
             }
-
-            var outputString = eventList.join(',  ');                    //turn events into string to be outputted through Dialogflow
-
-            app.tell("Here is a list of events happening in " + city + ": " + outputString); //provide response to user
-
-        }).catch(function (err) {
-            console.log("Error Occurred :" + err);          //catch errors from SkiddleEvents function
         });
 
-    }).catch(function(err){
-        console.log('error occurred: ' + err)               //catch errors from geodata function
-        }
-    )
-  }
-
-  function findArtist (app){
-    let artist = app.getArgument(ARTIST_ARGUMENT);
 
 
-    findSkiddleArtist(artist).then(function(res){
-        var data = res;
 
 
-        var results = JSON.parse(data).results;
-        var pageCount = JSON.parse(data).pagecount;
+        //    app.tell('You wont be forgotten ' + name + ". \xa0" + "The genres you like are : " + genres + " The artists you like are: " + artists );
+
+    }
 
 
-        var eventDict = {};             //dictionary of events, where key is the venue name and value is the act/name of the event
 
-        for(let i = 0; i<pageCount;i++) {
-            eventDict[results[i]['venue']['name']] = results[i]['eventname'];      //fill dictionary with venue and event names
-        }
+    function findBasicEvent (app) {
+
+        let city = app.getArgument(CITY_ARGUMENT);          //get specified city argument from dialogFlow
+
+        getGeoData(city).then(function(r){                  //get the geodata from the specified city using Google API
+            var data = r;
+
+            JSON.stringify(data);
+
+            //get latitude and longitude from geodata
+            var latitude = data[0]['latitude'];
+            var longitude = data[0]['longitude'];
 
 
-        var eventList = []                  //list to hold venues and events names as list
+            getBasicSkiddleEvents(latitude,longitude).then(function (res) {     //use latitude and longitude with skiddle API to return an events object
+                var data = res;
 
+                var results = JSON.parse(data).results;                    //get results of events object
+                var pageCount = JSON.parse(data).pagecount;                //get page count
 
-        for (var key in eventDict) {
-            if (eventDict.hasOwnProperty(key)) {
-                eventList.push( [key, eventDict[key]]);         //put each list of venue and event name in to list
+                var eventList = [];                                         //list to hold event names
+
+                for(let i = 0; i<pageCount;i++){
+                    eventList.push(results[i]['eventname']);                //put events in list
+                }
+
+                var outputString = eventList.join(',  ');                    //turn events into string to be outputted through Dialogflow
+
+                app.tell("Here is a list of events happening in " + city + ": " + outputString); //provide response to user
+
+            }).catch(function (err) {
+                console.log("Error Occurred :" + err);          //catch errors from SkiddleEvents function
+            });
+
+        }).catch(function(err){
+                console.log('error occurred: ' + err)               //catch errors from geodata function
             }
-        }
+        )
+    }
 
-        var eventFormattedList = [];            //list to hold formatting
-
-
-        for (var i = 0; i <eventList.length; i++){
-            eventFormattedList.push(eventList[i].join(', is Hosting: '));      //format each list to say "venue, is Hosting: event"
-        }
-
-        var outputString = eventFormattedList.join(", \xa0");               //format string to be outputted
-
-        app.tell(outputString);                             //provide response to user
+    function findArtist (app){
+        let artist = app.getArgument(ARTIST_ARGUMENT);
 
 
-    }).catch(function(e){
-        console.log("Error Occurred: "+e);
-    })
-
-  }
-
-  // d. build an action map, which maps intent names to functions
-  let actionMap = new Map();
-  actionMap.set(WELCOME_FIRST_TIME_ACTION,welcomeFirstTime);
-  actionMap.set(FIND_BASIC_EVENTS_ACTION,findBasicEvent);
-  actionMap.set(FIND_ARTIST,findArtist);
+        findSkiddleArtist(artist).then(function(res){
+            var data = res;
 
 
-app.handleRequest(actionMap);
+            var results = JSON.parse(data).results;
+            var pageCount = JSON.parse(data).pagecount;
+
+
+            var eventDict = {};             //dictionary of events, where key is the venue name and value is the act/name of the event
+
+            for(let i = 0; i<pageCount;i++) {
+                eventDict[results[i]['venue']['name']] = results[i]['eventname'];      //fill dictionary with venue and event names
+            }
+
+
+            var eventList = []                  //list to hold venues and events names as list
+
+
+            for (var key in eventDict) {
+                if (eventDict.hasOwnProperty(key)) {
+                    eventList.push( [key, eventDict[key]]);         //put each list of venue and event name in to list
+                }
+            }
+
+            var eventFormattedList = [];            //list to hold formatting
+
+
+            for (var i = 0; i <eventList.length; i++){
+                eventFormattedList.push(eventList[i].join(', is Hosting: '));      //format each list to say "venue, is Hosting: event"
+            }
+
+            var outputString = eventFormattedList.join(", \xa0");               //format string to be outputted
+
+            app.tell(outputString);                             //provide response to user
+
+
+        }).catch(function(e){
+            console.log("Error Occurred: "+e);
+        })
+
+    }
+
+    // d. build an action map, which maps intent names to functions
+    let actionMap = new Map();
+    actionMap.set(SPOTIFY_LOGGED_IN,spotifyLoggedIn);
+    actionMap.set(WELCOME_PICK_USERNAME,welcomePickUsername);
+    actionMap.set(FIND_BASIC_EVENTS_ACTION,findBasicEvent);
+    actionMap.set(FIND_ARTIST,findArtist);
+
+
+    app.handleRequest(actionMap);
 });
-
-
-
-
-
-
-
-
-
